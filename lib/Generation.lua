a1139e0f87fda97a33a1a2417ac02a70b1fdd55f
aud1tproof <zx4sfg4@gmail.com>
2026-05-26 13:04:57 +0200

    Update Generation.lua

--- lib/Generation.lua
+++ lib/Generation.lua
@@ -5,7 +5,7 @@ type table = {
 }
 
 --// Libraries
-local ParserModule = loadstring(game:HttpGet('https://raw.githubusercontent.com/depthso/Roblox-parser/refs/heads/main/main.lua'))()
+local ParserModule = loadstring(game:HttpGet('https://raw.githubusercontent.com/aud1tproof/Sigma-Spy/refs/heads/main/lib/parser.lua'))()
 
 --// Parser
 function ParserModule:Import(Name: string)
