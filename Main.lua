9fe3a44107943312575b1eb6ae3a63be49cc4a19
aud1tproof <zx4sfg4@gmail.com>
2026-05-26 12:51:00 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -20,7 +20,7 @@
 --// File handling configuration 
 local FilesConfig = {
 	UseWorkspace = false,
-	RepoUrl = "https://raw.githubusercontent.com/depthso/Sigma-Spy/refs/heads/main" -- "http://127.0.0.1:3000"
+	RepoUrl = "https://raw.githubusercontent.com/aud1tproof/Sigma-Spy/refs/heads/main" -- "http://127.0.0.1:3000"
 }
 
 --// Service handlers
